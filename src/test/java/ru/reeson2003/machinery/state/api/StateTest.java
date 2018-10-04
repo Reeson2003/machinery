3be9009d44f861a7d9b32a3299c71e00736b5d7f
pavel.gavrilov <pavel.gavrilov@binomo.com>
2018-10-04 12:36:24 +0300

    readme add

---
+++ src/test/java/ru/reeson2003/machinery/state/api/StateTest.java
@@ -0,0 +1,15 @@
+package ru.reeson2003.machinery.state.api;
+
+import org.junit.Test;
+
+import static org.hamcrest.CoreMatchers.notNullValue;
+import static org.junit.Assert.assertThat;
+
+public class StateTest {
+
+    @Test
+    public void createTest() {
+        State<String, Action<?>> test = State.create("TEST", (s, a) -> s);
+        assertThat(test, notNullValue());
+    }
+}
