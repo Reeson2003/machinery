2f24f791842262386232680de5b0302c2c65c228
reeson2003 <reeson2003@gmail.com>
2018-10-10 22:54:19 +0300

    single tread executor for actions

--- src/main/java/ru/reeson2003/machinery/impl/StateImpl.java
+++ src/main/java/ru/reeson2003/machinery/impl/StateImpl.java
@@ -6,6 +6,8 @@ import java.lang.reflect.Field;
 import java.lang.reflect.Modifier;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 public class StateImpl<S, A extends Action<?, ?>>
         implements State<S, A> {
@@ -15,6 +17,8 @@ public class StateImpl<S, A extends Action<?, ?>>
     private Dispatcher<S, A> dispatcher;
 
     private List<StateListener<S>> listeners = new ArrayList<>();
+
+    private ExecutorService executor = Executors.newSingleThreadExecutor();
 
     public StateImpl(S state, Dispatcher<S, A> dispatcher) {
         checkFinality(state);
@@ -35,9 +39,15 @@ public class StateImpl<S, A extends Action<?, ?>>
 
     @Override
     public void perform(A action) {
-        this.state = dispatcher.dispatch(state, action);
-        for (StateListener<S> l : listeners)
-            l.update(this.state);
+        execute(() -> {
+            this.state = dispatcher.dispatch(state, action);
+            for (StateListener<S> l : listeners)
+                l.update(this.state);
+        });
+    }
+
+    private void execute(Runnable runnable) {
+        executor.submit(runnable);
     }
 
     @Override
